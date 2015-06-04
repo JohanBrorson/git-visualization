b127e79e1d1b73e24853fefc365bf78aa1f40c76
Johan Brorson <johan_brorson@hotmail.com>
2015-06-04 14:02:59 +0200

    Fix script to handle dates with no commits

--- git-commits-between.sh
+++ git-commits-between.sh
@@ -24,8 +24,25 @@ function usage {
   exit 1
 }
 
+function get_next_date() {
+  FORMATTED_DATE=`date -d "@$1" +"$DATE_FORMAT_DATE"`
+  date -d "$FORMATTED_DATE +1 $GROUP_BY_STRING" +%s
+}
+
+function get_epoch() {
+  date -d "$1" +%s
+}
+
+function get_formatted_date() {
+  date -d "@$1" +"$DATE_FORMAT"
+}
+
+function get_week() {
+  date -d "$(( $(date -d "$1" +%u) - 1 + $(( ($(date -d "today" +%s) - $(date -d "$1" +%s) )/(60*60*24) )))) days ago" +"%Y-%m-%d %V"
+}
+
 # Parse command line arguments
-while getopts ":s:u:g:" OPTION
+while getopts "s:u:g:" OPTION
 do
   case $OPTION in
     s   ) FROM=$OPTARG;;
@@ -64,20 +81,39 @@ esac
 shopt -u extglob nocasematch
 
 
+FROM_EPOCH=`get_epoch "$FROM"`
+TO_EPOCH=`get_epoch "$TO"`
+DATES=""
 
+# Get all dates
+while [ $FROM_EPOCH -lt $TO_EPOCH ]; do
+  FROM_FORMATTED="`get_formatted_date $FROM_EPOCH`"
+  if [[ "$GROUP_BY_STRING" == "week" ]]
+  then
+    DATES+="`get_week $FROM_FORMATTED`\n"
+  else
+    DATES+="$FROM_FORMATTED\n"
+  fi
+  NEXT_DAY=`get_next_date $FROM_EPOCH`
+  FROM_EPOCH=$NEXT_DAY
+done
+
+# Get dates with commits
 if [[ "$GROUP_BY_STRING" == "week" ]]
-then  
-  git log --since "$FROM" --until "$TO" --format="%ct" --reverse | \
+then
+  DATES+=`git log --since "$FROM" --until "$TO" --format="%ct" --reverse | \
     xargs -I{} date -d "@{}" +"$DATE_FORMAT_WEEK" | \
-    xargs -I DATE bash -c 'date -d "$(( $(date -d "DATE" +%u) - 1 + $(( ($(date -d "today" +%s) - $(date -d "DATE" +%s) )/(60*60*24) )))) days ago" +"$DATE_FORMAT_WEEK %W"' | \
-    uniq -c | \
-    sed 's/^\s*//' >$DAT_FILE
+    xargs -I COMMIT_DATE bash -c 'date -d "$(( $(date -d "COMMIT_DATE" +%u) - 1 + $(( ($(date -d "today" +%s) - $(date -d "COMMIT_DATE" +%s) )/(60*60*24) )))) days ago" +"%Y-%m-%d %V"'`
 else
-  git log --since "$FROM" --until "$TO" --format="%ct" --reverse | \
-    xargs -I{} date -d "@{}" +"$DATE_FORMAT" | \
-    uniq -c | \
-    sed 's/^\s*//' >$DAT_FILE
+  DATES+=`git log --since "$FROM" --until "$TO" --format="%ct" --reverse | \
+    xargs -I{} date -d "@{}" +"$DATE_FORMAT"`
 fi
+
+echo -n -e "$DATES" | \
+  sort | \
+  uniq -c | \
+  sed 's/^\s*//' | \
+  awk -F"[ ]" '{print $1 - 1 " " $2}' >$DAT_FILE
 
 echo "set title 'Number of commits per $GROUP_BY_STRING'"              > $PLOT_FILE
 echo "set key off"                                                    >> $PLOT_FILE
@@ -92,6 +128,7 @@ echo "set ytics nomirror"                                             >> $PLOT_F
 echo "set xtics nomirror rotate by -45"                               >> $PLOT_FILE
 echo "set xtics format \"$XTICS_FORMAT\""                             >> $PLOT_FILE
 echo "set tic scale 0"                                                >> $PLOT_FILE
+echo "set style fill solid 1.00 border"                               >> $PLOT_FILE
 echo "set terminal png nocrop enhanced font arial 12 size 1280,1024"  >> $PLOT_FILE
 echo "set output '$OUT_FILE'"                                         >> $PLOT_FILE
 echo "plot '$DAT_FILE' using 2:1 with boxes"                          >> $PLOT_FILE
@@ -105,7 +142,7 @@ gnuplot $PLOT_FILE
 if [ $? = 0 ]
 then
   # Clean up
-  echo "INFO: Success" 
+  echo "INFO: Successfully created $OUT_FILE"
   rm $PLOT_FILE
   rm $DAT_FILE
 else
